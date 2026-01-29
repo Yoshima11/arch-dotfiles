14c31390401b8703e381e393283cf5e043d57654
Yoshima11 <miked160581@gmail.com>
2026-01-29 16:54:48 -0300

    nvim transparente

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,8 @@
 require("core.options")
 require("lazy-setup")
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
+-- Transparencia:
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
